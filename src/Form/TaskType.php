3ddc4fb99f5f72b43c3ff1551dff7240d6e23754
AMooka <andrewmooka020@gmail.com>
2024-07-05 10:22:09 +0300

    Api documentation completed

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -8,17 +8,17 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface as FormFormBuilderInterface;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Date;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TaskType extends AbstractType
 {
-    public function buildForm(FormFormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder 
-        ->add('title', TextType::class, [
+        ->add('title', TextType::class, $options, [
             'label' => 'Title',
             'constraints' => [
             new NotBlank([
@@ -26,7 +26,7 @@ class TaskType extends AbstractType
             ]),
             ]
         ])
-        ->add('description', TextareaType::class, [
+        ->add('description', TextareaType::class, $options, [
             'label' => 'Description',
             'constraints' => [
             new NotBlank([
@@ -34,14 +34,14 @@ class TaskType extends AbstractType
             ]),
             ]
         ])
-        ->add('dueDate', DateType::class,[
+        ->add('dueDate', DateType::class, $options, [
             'constraints' => [
             new NotBlank([
                 'message' => 'Date can not be blank.',
             ])
             ]
         ])
-        ->add('status', ChoiceType::class, [
+        ->add('status', ChoiceType::class, $options, [
             'choices' => [
                 'Select Status' => 'select',
                 'To do' => 'To Do',
@@ -55,6 +55,7 @@ class TaskType extends AbstractType
             ]
         ]);
     }
+    
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver -> setDefaults([
